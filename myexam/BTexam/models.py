0844d0bb39542ac821d73dfb5bab85229a3c0746
git2 <email>
2025-05-30 11:41:17 +0300

    создал модель пункт 1

--- myexam/BTexam/models.py
+++ myexam/BTexam/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BTexam(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название экзамена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания записи")
+    exam_date = models.DateTimeField(verbose_name="Дата проведения экзамена")
+    image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания")
+    users = models.ManyToManyField(User, verbose_name="Пользователи, сдающие экзамен")
+    is_public = models.BooleanField(default=False, verbose_name="Опубликовано")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Экзамен"
+        verbose_name_plural = "Экзамены"

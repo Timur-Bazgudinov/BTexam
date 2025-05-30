256ceb39bf09aa0c569cd1f41b410a58832c7f9c
git2 <email>
2025-05-30 11:49:11 +0300

    последние штрихи пунктов 1-5

--- myexam/BTexam/views.py
+++ myexam/BTexam/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import BTexam
 
-# Create your views here.
+def BTexam_list(request):
+    exams = BTexam.objects.filter(is_public=True)
+    return render(request, 'BTexam/list.html', {
+        'exams': exams,
+        'fio': 'Базгудинов Тимур Айдарович',
+        'group': 'Группа 231-365'
+    })
--- myexam/myexam/urls.py
+++ myexam/myexam/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import BTexam_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+
+
+
+
+urlpatterns = [
+    path('ipexam/', BTexam_list, name='ipexam_list'),
 ]

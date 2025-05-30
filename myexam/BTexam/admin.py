6e73473a472ad379d2e118c502ac1448774838ac
git2 <email>
2025-05-30 11:43:15 +0300

    админка пункт 2

--- myexam/BTexam/admin.py
+++ myexam/BTexam/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BTexam
+from django.contrib.auth.models import User
 
-# Register your models here.
+@admin.register(BTexam)
+class ipexamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'exam_date', 'is_public')
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('title', 'users__email') 
+    filter_horizontal = ('users',) 
+
+    date_hierarchy = 'exam_date'
